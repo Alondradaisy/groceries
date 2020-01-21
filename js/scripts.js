02a15bf1391142f8540f6f9d8031870fff4f3888
Alex Skreen, Alondra Warren and Dusty McCord <dusty@>
2020-01-21 15:25:04 -0800

    fix the toggle of showing and hiding the inputs.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,50 @@
 $(document).ready(function () {
   var listItems = []
-  var printOut = $("#list1")
+  var printOut = $("#list1");
+  var show = true;
 
-  $("#submitButton").click(function(event){
-  event.preventDefault();
-  var groceryItem = $("input#groceryItem").val();
-  listItems.push(groceryItem);
-  listItems.sort();
-  printOut.empty();
+  $("#submitButton").click(function (event) {
+    event.preventDefault();
 
-  listItems.forEach(function(item){
-    $("#list1").append("<li> " + item + "</li>")
-  })
+    //makes input array
+    var allItems = $('input.item');
 
-  })
+    //adds inputs to array
+    allItems.each(function (index, item) {
+      listItems.push(item.value);
+    });
+
+    // listItems.push(groceryItem);
+    listItems.sort();
+    printOut.empty();
+    toggleShow();
+
+  }); // END BUTTON CLICK
+
+  $("#bringListBack").click(function (event) {
+    toggleShow();
+  });
+
+
+  function toggleShow() {
+    if (show) {
+      $('#hide-me').toggle();
+      $('#show-me').toggle();
+
+      //Prints out items
+      listItems.forEach(function (item) {
+        $("#list1").append("<li> " + item + "</li>")
+      })
+    } else if (!show) {
+      
+        $('#show-me').toggle();
+        $('#hide-me').toggle();
+        $('#bringListBack').hide();
+
+        console.log("button works")
+    } else {
+      console.log('error');
+    }
+  }
+
 });
